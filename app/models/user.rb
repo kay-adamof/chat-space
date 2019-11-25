b6a42f906b9d32793a7986c6caa3fb2fe03dc9e2
kay-adamof <57440326+kay-adamof@users.noreply.github.com>
2019-11-25 18:58:28 +0900

    no change

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+<<<<<<< HEAD
 
+=======
+>>>>>>> parent of c95fb97... ユーザー認証チェック追加 on user.rb
 end
