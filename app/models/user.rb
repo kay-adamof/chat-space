b4be0083a63b010b1245467de24d38e45bd9b660
kay-adamof <57440326+kay-adamof@users.noreply.github.com>
2019-11-25 19:24:43 +0900

    ユーザー認証追加（再）
    user.rbにbefore_action :authenticate_user!追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-<<<<<<< HEAD
-
-=======
->>>>>>> parent of c95fb97... ユーザー認証チェック追加 on user.rb
+  before_action :authenticate_user!
 end
